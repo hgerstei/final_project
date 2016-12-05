e447f4f97686ddd3f1db05e3d62a354f351db523
Raghu Betina <raghu@firstdraft.com>
2016-12-05 20:22:31 +0000

    Validate presence of purchases.purchase_date

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -5,6 +5,8 @@ class Purchase < ApplicationRecord
 
   # Validations
 
+  validates :purchase_date, :presence => true
+
   validates :purchase_name, :uniqueness => { :scope => [:purchase_date, :category_id] }
 
   validates :purchase_name, :presence => true
